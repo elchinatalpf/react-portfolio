9028334994a78426d3816a20fbcde23001ea648f
Javier <elchinatalpf@github.com>
2023-10-31 20:26:34 -0500

    Added navigation logic

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,9 +5,45 @@ function NavTabs () {
 
   return (
     <nav>
-      <ul>
-        <li>
-          <Link>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <Link
+          to="/"
+          className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+          >
+          Home
+          </Link>
+        </li>
+        <li className="nav-item">
+        <Link
+        to="/AboutMe"
+        className={currentPage === '/AboutMe' ? 'nav-link active' : 'nav-link'}
+        >
+          About Me
+          </Link>
+        </li>
+        <li className="nav-item">
+        <Link
+        to="/Portfolio"
+        className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+          </Link>
+        </li>
+        <li className="nav-item">
+        <Link
+        to="/Resume"
+        className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+          </Link>
+        </li>
+        <li className="nav-item">
+        <Link
+        to="/Contact"
+        className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
           </Link>
         </li>
       </ul>
@@ -15,3 +51,5 @@ function NavTabs () {
   )
 
 }
+
+export default NavTabs;
