888f21c0f0b3daf1d46c311f7d034bcdf97e2728
Javier <elchinatalpf@github.com>
2023-11-06 15:40:43 -0600

    updated contact with validation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,30 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import emailjs from '@emailjs/browser';
-import env from 'react-dotenv';
 
-const serviceId = import.meta.env.REACT_APP_SERVICE_ID;
-const formId = import.meta.env.REACT_APP_FORM_ID;
-const publicKey = import.meta.env.REACT_APP_USER_ID;
+const serviceId = import.meta.env.VITE_REACT_APP_SERVICE_ID;
+const formId = import.meta.env.VITE_REACT_APP_FORM_ID;
+const publicKey = import.meta.env.VITE_REACT_APP_USER_ID;
+
+export default function Contact() {
+
+  const [ name, setName ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ message, setMessage ] = useState('');
+  const [ validationErrors, setValidationErrors ] = useState({});
+
+  const validateForm = () => {
+    const errors = {};
+
+    if (!name) {
+      errors.name = "Your name is required";
+    }
+
+    if (!email || !/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(email)) {
+      errors.email = "Your email address is invalid";
+    }
+
+    if (!message) {
+      errors.message = "Your message is required";
+    } 
+
+    setValidationErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
 
 const sendEmail = async (event) => {
   event.preventDefault();
 
+  if (!validateForm()) {
+    return;
+  }
+
   try {
     const result = await emailjs.sendForm(
-      import.meta.env.VITE_REACT_APP_SERVICE_ID,
-      import.meta.env.VITE_REACT_APP_FORM_ID,
+      serviceId,
+      formId,
       event.target,
-      import.meta.env.VITE_REACT_APP_USER_ID,
+      publicKey,
     );
 
     event.target.reset();
+
+    setName('');
+    setEmail('');
+    setMessage('');
+
     console.log(result);
   } catch (err) {
     console.log(err);
   }
 };
 
-export default function Contact() {
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    switch (name) {
+      case 'from_name':
+        setName(value);
+        break;
+      case 'user_email':
+        setEmail(value);
+        break;
+      case 'message':
+        setMessage(value);
+          break;
+    }
+    
+  };
+  
+  //     import.meta.env.VITE_REACT_APP_SERVICE_ID,
+  //     import.meta.env.VITE_REACT_APP_FORM_ID,
+  //     import.meta.env.VITE_REACT_APP_USER_ID,
+
 
   return (
     <div>
@@ -37,25 +90,43 @@ export default function Contact() {
             <form className="contact-form" onSubmit={sendEmail}>
               <div className="mb-3">
                 <label htmlFor="user-name">Name</label>
-                <input type="text" className="user-name form-control" name="from_name" required />
+                <input
+                type="text"
+                className="user-name form-control"
+                name="from_name"
+                value={name}
+                onChange={handleChange}
+                required />
               </div>
 
               <div>
                 <label htmlFor="user-email" className="user-email" name="user-email">
                   Email
                 </label>
-                <input type="text" className="user-email form-control" name="user_email" required />
+                <input
+                type="text"
+                className="user-email form-control"
+                name="user_email"
+                value={email}
+                onChange={handleChange}
+                required />
               </div>
 
               <div className="contact-form">
                 <label htmlFor="message" className="user-message">
                   Message
                 </label>
-                <textarea className="message form-control" name="message" required></textarea>
+                <textarea
+                className="message form-control"
+                name="message"
+                value={message}
+                onChange={handleChange}
+                required></textarea>
               </div>
 
               <div>
-                <input type="submit" value="send" className="btn btn-primary" />
+                <input
+                type="submit" value="send" className="btn btn-primary" />
               </div>
             </form>
             
